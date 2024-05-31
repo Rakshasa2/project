9578e6e98c3e2bd39334ba16049537effadf29f6
Матвей Надводнюк <145839983+br0kenr1bs@users.noreply.github.com>
2024-05-31 15:23:23 +0300

    Add files via upload

---
+++ main.go
@@ -0,0 +1,115 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"package/db"
+	"package/handlers"
+	"syscall"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+
+// основная исполняющая функция, здесь мы инициализируем БД, настраиваем маршруты запросов
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	databaseURL := os.Getenv("DATABASE_URL")
+	pool := db.DbStart(databaseURL)
+
+	db := db.NewDB(pool)
+	handler := handlers.NewBaseHandler(db)
+	r := gin.Default()
+
+	// здесь игнорим CORS
+	r.Use(func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(http.StatusOK)
+			return
+		}
+		c.Next()
+	})
+	// роут получения инфы о пользователе
+	r.GET("/getUserInfo", func(c *gin.Context) {
+		handler.GetUserInfo(c)
+	})
+	v1 := r.Group("/auth")
+	{
+		// роут отправки подтверждающего письма на почту
+		v1.POST("/sendMail", func(c *gin.Context) {
+			handler.SendMail(c)
+		})
+		// роут регистрации
+		v1.POST("/register", func(c *gin.Context) {
+			handler.RegisterUser(c)
+		})
+		// роут входа
+		v1.POST("/login", func(c *gin.Context) {
+			handler.LoginUser(c)
+		})
+	}
+
+	user := r.Group("/user")
+	{
+		// роут изменения данных пользователя
+		user.PUT("/edit", func(c *gin.Context) {
+			handler.EditUser(c)
+		})
+	}
+
+	card := r.Group("/task")
+	{
+		// роут создания карточки
+		card.POST("/create", func(c *gin.Context) {
+			handler.CreateTask(c)
+		})
+		// роут удаления карточки
+		card.DELETE("/delete", func(c *gin.Context) {
+			handler.DeleteTask(c)
+		})
+		// роут получения всех карточек
+		card.GET("/getAll", func(c *gin.Context) {
+			handler.GetAllTasks(c)
+		})
+	}
+	//остальное базовые настройки сервера
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: r,
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	quit := make(chan os.Signal)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("Shutdown Server ...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Server Shutdown:", err)
+	}
+	// catching ctx.Done(). timeout of 5 seconds.
+	select {
+	case <-ctx.Done():
+		log.Println("timeout of 5 seconds.")
+	}
+	log.Println("Server exiting")
+}
